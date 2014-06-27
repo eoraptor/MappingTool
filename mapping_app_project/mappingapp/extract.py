8403600a0424d73fe7c1b491f53b6fc00e03a7c8
Fiona Steven <9148031S@student.gla.ac.uk>
2014-06-27 10:34:26 +0100

    0.0.19
    Complete set of methods for extracting data from a TCN file.  Model
    object instantiation not included.

--- mapping_app_project/mappingapp/extract.py
+++ mapping_app_project/mappingapp/extract.py
@@ -1,95 +1,8 @@
 from openpyxl import load_workbook
-from mappingapp.models import Sample_Site, Coordinates
 
-def get_tcn_sample_info(wb, ws):
-    sample_sheet = wb[ws]
-    sample_date = sample_sheet['D1'].value
-    sample_location = sample_sheet['D2'].value
-    sample_code = sample_sheet['B3'].value
-    bng_ing = sample_sheet['D3'].value
-    sample_northing = sample_sheet['B4'].value
-    sample_easting = sample_sheet['D4'].value
-    transect = sample_sheet['F4'].value
-    latitude = sample_sheet['B5'].value
-    longitude = sample_sheet['D5'].value
-    elevation = sample_sheet['F5'].value
-    lithology = sample_sheet['B7'].value
-    quartz = sample_sheet['F7'].value
-    setting = sample_sheet['B8'].value
-    material = sample_sheet['F8'].value
-    boulder_dim = sample_sheet['B9'].value
-    surface_strike = sample_sheet['F9'].value
-    notes = sample_sheet['C11'].value
-    thickness = sample_sheet['D24'].value
-    grain_size = sample_sheet['F24'].value
-    collector = sample_sheet['F31'].value
-    bearing = get_bearing(sample_sheet)
+# iterate over the site sheet to determine which cells to extract data from
+def get_site_cell_positions(ws):
 
-    notes = notes.replace('\n', ' ')
-
-
-def get_bearing(sample_sheet):
-    data = []
-    done = False
-    start = 11
-    while not done:
-        start_str = str(start)
-        bearing = sample_sheet['A' + start_str].value
-        inclination = sample_sheet['B' + start_str].value
-
-        if bearing is not None or inclination is not None:
-            data.append((bearing, inclination))
-            start += 1
-        else:
-            return data
-
-
-
-def get_site_info(wb):
-    site_sheet = wb["Site Info"]
-    site_name = site_sheet['D1'].value
-    site_location = site_sheet['B3'].value
-    site_latitude = site_sheet['B4'].value
-    site_longitude = site_sheet['D4'].value
-    bng_ing = site_sheet['F4'].value
-    site_northing = site_sheet['B5'].value
-    site_easting = site_sheet['D5'].value
-    site_elevation = site_sheet['B6'].value
-    sample_date = site_sheet['E6'].value
-    geomorph = site_sheet['B7'].value
-    type = site_sheet['B8'].value
-    collector = site_sheet['E8'].value
-    photographs = site_sheet['B9'].value
-    photo_labels = site_sheet['D9'].value
-    site_notes = site_sheet['B10'].value
-
-    photographs = photographs.lower()
-    if photographs.startswith('y'):
-        photographs = True
-    else:
-        photographs = False
-
-    coords = Coordinates.objects.create_coordinates(bng_ing, None, site_easting, site_northing, site_latitude, site_longitude, site_elevation)
-    site = Sample_Site.objects.create_site(site_name, site_location, None, geomorph, type, photographs, site_notes, None, None, coords)
-
-
-
-
-def process_file(file_name):
-    wb = load_workbook(file_name)
-    sheet_names = wb.get_sheet_names()
-
-    for sheet in sheet_names:
-        if sheet == 'Site Info':
-            get_site_info(wb)
-
-        else:
-            get_tcn_sample_info(wb, sheet)
-
-
-
-def get_site_cell_positions(wb):
-    ws = wb['Site Info']
     columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -110,10 +23,42 @@ def get_site_cell_positions(wb):
                     val_col = columns[col+1]
                     positions[val] = val_col + str(cell.row)
 
-    for k,v in positions.items():
-        print k,v
+    return positions
+
+# extract the data
+def get_site_info(wb):
+    site_sheet = wb["Site Info"]
+    positions = get_site_cell_positions(site_sheet)
+
+    site_name = site_sheet[positions['Site Name: ']].value
+    site_location = site_sheet[positions['Location (inc. Transect no.)']].value
+    site_latitude = site_sheet[positions['Latitude(decimal deg)']].value
+    site_longitude = site_sheet[positions['Longtitude(decimal deg)(West is -ve)']].value
+    bng_ing = site_sheet[positions['BNG/ING?']].value
+    site_northing = site_sheet[positions['Northing:']].value
+    site_easting = site_sheet[positions['Easting:']].value
+    site_elevation = site_sheet[positions['Elevation             (m ASL)']].value
+    sample_date = site_sheet[positions['Date Sampled:']].value
+    geomorph = site_sheet[positions['Geomorph Setting:']].value
+    type = site_sheet[positions['Type of Samples collected (TCN/14C/OSL)']].value
+    collector = site_sheet[positions['Collected by:']].value
+    photographs = site_sheet[positions['Photographs Taken (Y/N):']].value
+    photo_labels = site_sheet[positions['Photo labels/Time stamps:']].value
+    site_notes = site_sheet[positions['Notes']].value
+
+    photographs = photographs.lower()
+    if photographs.startswith('y'):
+        photographs = True
+    else:
+        photographs = False
+
+    print site_name, '\n', site_location, '\n', site_latitude, '\n', site_longitude, '\n', bng_ing, '\n', site_northing
+    print site_easting, '\n', site_elevation, '\n', sample_date, '\n', geomorph, type, '\n', collector
+    print photographs, '\n', photo_labels, '\n', site_notes
 
 
+
+# iterate over the tcn sheet to determine which cells to extract data from
 def get_tcn_cell_positions(ws):
     columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -140,8 +85,75 @@ def get_tcn_cell_positions(ws):
                         val_col = columns[col+1]
                         positions[val] = val_col + str(cell.row)
 
-    for k,v in positions.items():
-        print k,v
+    return positions
 
-#ws = wb['T1FOU02']
-#get_tcn_cell_positions(ws)
+
+# extract the data from a tcn sample sheet
+def get_tcn_sample_info(sample_sheet):
+    positions = get_tcn_cell_positions(sample_sheet)
+
+    sample_date = sample_sheet[positions['Date: ']].value
+    sample_location = sample_sheet[positions['Location:']].value
+    sample_code = sample_sheet[positions['Unique Sample Identifier']].value
+    bng_ing = sample_sheet[positions['BNG or ING']].value
+    sample_northing = sample_sheet[positions['Northing']].value
+    sample_easting = sample_sheet[positions['Easting']].value
+    transect = sample_sheet[positions['Transect:']].value
+    latitude = sample_sheet[positions['Latitude (if different from site info)']].value
+    longitude = sample_sheet[positions['Longtitude']].value
+    elevation = sample_sheet[positions['Elevation']].value
+    lithology = sample_sheet[positions['Lithology']].value
+    quartz = sample_sheet[positions['Est Quartz content']].value
+    setting = sample_sheet[positions['Sample setting']].value
+    material = sample_sheet[positions['Sampled material (eg:boulder)']].value
+    boulder_dim = sample_sheet[positions['Boulder dimensions [cm] (LxWxH)']].value
+    surface_strike = sample_sheet[positions['Sample surface strike/dip ']].value
+    notes = sample_sheet[positions['Notes (inc.weathering and erosion rate est)']].value
+    thickness = sample_sheet[positions['sample thickness']].value
+    grain_size = sample_sheet[positions['Grain Size:']].value
+    collector = sample_sheet[positions['Collector: ']].value
+    bearing = get_bearing(sample_sheet, positions['Bearing'])
+
+    notes = notes.replace('\n', ' ')
+
+    print sample_date, '\n', sample_location, '\n', sample_code, '\n', bng_ing, '\n', sample_northing, '\n', sample_easting, '\n', transect, '\n', latitude
+    print longitude, '\n', elevation, '\n', lithology, '\n', quartz, '\n', setting, '\n', material, boulder_dim, '\n', surface_strike, '\n', notes, '\n', thickness, '\n', grain_size, '\n', collector
+    for b in bearing:
+            print b[0],b[1]
+    print '\n'
+
+
+
+# extract all bearing and inclination data from tcn sheets
+def get_bearing(sample_sheet, start_cell):
+    data = []
+    done = False
+    start = int(start_cell[1:])
+    while not done:
+        start_str = str(start)
+        bearing = sample_sheet['A' + start_str].value
+        inclination = sample_sheet['B' + start_str].value
+
+        if bearing is not None or inclination is not None:
+            data.append((int(bearing), int(inclination)))
+            start += 1
+        else:
+            return data
+
+
+# process a complete file
+def process_file(filename):
+    # need error handling
+    wb = load_workbook(filename, use_iterators=True)
+
+    sheet_names = wb.get_sheet_names()
+
+    for sheet in sheet_names:
+        if sheet != 'Site Info':
+            ws = wb[sheet]
+            get_tcn_sample_info(ws)
+
+
+
+
+#process_file("C:\\Users\\fstev_000\\Documents\\T1 sample info.xlsx")
