0f151965a97025c0ba68bbfc41e4b013c6207587
Fiona Steven <9148031S@student.gla.ac.uk>
2014-08-05 11:45:02 +0100

    0.1.64
    Ability to draw and remove highlighted areas on map added.

--- mapping_app_project/static/js/googlemaps.js
+++ mapping_app_project/static/js/googlemaps.js
@@ -1,8 +1,31 @@
 var map;
 var markers = [];
 var infowindow = null;
+var drawingManager;
 var mapicons = [];
+var selectedShape;
 var iconBase = "/static/imgs/";
+
+     function clearSelection() {
+        if (selectedShape) {
+          selectedShape.setEditable(false);
+          selectedShape = null;
+        }
+      }
+
+      function setSelection(shape) {
+        clearSelection();
+        selectedShape = shape;
+        shape.setEditable(true);
+      }
+
+      function deleteSelectedShape() {
+        if (selectedShape) {
+          selectedShape.setMap(null);
+        }
+      }
+
+
 var icons = {
     osl: {
         name: 'OSL  ',
@@ -18,8 +41,7 @@ var icons = {
     }
 };
 
-(function() {
-    window.onload = function() {
+function initialize() {
 
         make_icons();
 
@@ -40,7 +62,53 @@ var icons = {
 
         map = new google.maps.Map(mapDiv, map_options);
 
-                var legend = document.getElementById('map-legend');
+        drawingManager = new google.maps.drawing.DrawingManager({
+            drawingControl: true,
+            drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT,
+            drawingModes: [
+            google.maps.drawing.OverlayType.CIRCLE,
+            google.maps.drawing.OverlayType.POLYGON,
+            google.maps.drawing.OverlayType.RECTANGLE
+          ]
+        },
+        polygonOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 0.4,
+            strokeWeight: 3,
+            clickable: true,
+            zIndex: 1,
+            editable: true,
+            draggable:true
+        },
+        circleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 0.4,
+            strokeWeight: 3,
+            clickable: true,
+            zIndex: 1,
+            editable: true,
+            draggable:true
+        },
+        rectangleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 0.4,
+            strokeWeight: 3,
+            clickable: true,
+            zIndex: 1,
+            editable: true,
+            draggable:true
+        },
+            map: map
+        });
+
+        google.maps.event.addListener(drawingManager, 'polygoncomplete', function (polygon) {
+            var coordinates = (polygon.getPath().getArray());
+            console.log(coordinates);
+        });
+
+
+        var legend = document.getElementById('map-legend');
 
         for (var key in icons) {
             var type = icons[key];
@@ -58,6 +126,32 @@ var icons = {
         function calculateCenter() {
             center = map.getCenter();
         }
+
+        var clearButtonDiv = document.createElement('div');
+        var clear = new clearButton(clearButtonDiv, map);
+
+        clearButtonDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_TOP].push(clearButtonDiv);
+
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
+
+        // Switch back to non-drawing mode after drawing a shape.
+        drawingManager.setDrawingMode(null);
+
+        // Add an event listener that selects the newly-drawn shape when the user
+        // mouses down on it.
+        var newShape = e.overlay;
+        newShape.type = e.type;
+        google.maps.event.addListener(newShape, 'click', function() {
+          setSelection(newShape);
+        });
+        setSelection(newShape);
+
+        // Clear the current selection when clear button clicked
+        google.maps.event.addDomListener(document.getElementById("clearbutton"), 'click', deleteSelectedShape);
+        google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
+        google.maps.event.addListener(map, 'click', clearSelection);
+    });
 
         google.maps.event.addDomListener(map, 'idle', function () {
             calculateCenter();
@@ -101,15 +195,14 @@ var icons = {
                 infowindow.open(map, marker);
                 });
                 })(i,marker);
-        }
-})();
+            }
+    })();
 
 //        bottom of function
-    };
-})();
+};
 
 
-
+google.maps.event.addDomListener(window, 'load', initialize);
 
 var make_icons = function() {
     mapicons['tcn'] = new google.maps.MarkerImage(
@@ -136,3 +229,32 @@ var make_icons = function() {
         new google.maps.Size(28, 28)
     )
 };
+
+
+function clearButton(controlDiv, map) {
+
+  // Set CSS styles for the DIV containing the control
+  // Setting padding to 5 px will offset the control
+  // from the edge of the map
+  controlDiv.style.padding = '5px';
+
+  // Set CSS for the control border
+  var controlUI1 = document.createElement('div');
+  controlUI1.id = "clearbutton";
+  controlUI1.style.backgroundColor = 'white';
+  controlUI1.style.borderStyle = 'solid';
+  controlUI1.style.borderWidth = '1px';
+  controlUI1.style.cursor = 'pointer';
+  controlUI1.style.textAlign = 'center';
+  controlUI1.title = 'Click to delete shape';
+  controlDiv.appendChild(controlUI1);
+
+  // Set CSS for the control interior
+  var controlText1 = document.createElement('div');
+  controlText1.style.fontFamily = 'Arial,sans-serif';
+  controlText1.style.fontSize = '12px';
+  controlText1.style.paddingLeft = '4px';
+  controlText1.style.paddingRight = '4px';
+  controlText1.innerHTML = '<b>Delete Shape</b>';
+  controlUI1.appendChild(controlText1);
+}
